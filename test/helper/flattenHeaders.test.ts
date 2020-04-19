4caaca10ad029acd5e93c43ad7318bdac4ab594d
954270063@qq.com <954270063@qq.com>
2020-04-19 16:31:12 +0800

    1.0.6

--- test/helper/flattenHeaders.test.ts
+++ test/helper/flattenHeaders.test.ts
@@ -2,15 +2,13 @@
  * @Author: early-autumn
  * @Date: 2020-04-19 14:43:15
  * @LastEditors: early-autumn
- * @LastEditTime: 2020-04-19 16:19:24
+ * @LastEditTime: 2020-04-19 16:30:57
  */
 import flattenHeaders from '../../src/helper/flattenHeaders';
 
 describe('测试 src/helper/flattenHeaders.ts', () => {
   it('测试 容错', () => {
-    expect(flattenHeaders({})).toEqual({
-      Accept: 'application/json, test/plain, */*',
-    });
+    expect(flattenHeaders({})).toEqual({});
     //  'Content-Type': 'application/json; charset=utf-8',
   });
 });
