48658e7618bea02213934cb6e7b852e36f4b44ee
zjx0905 <954270063@qq.com>
2020-07-27 11:39:27 +0800

    clear

--- __tests__/helpers/combineURL.spec.ts
+++
@@ -1,8 +0,0 @@
-import combineURL from '../../src/helpers/combineURL';
-
-describe('测试 src/helpers/combineURL.ts', () => {
-  it('run', () => {
-    expect(combineURL('1/2', '3/4')).toBe('1/2/3/4');
-    expect(combineURL('1/2///', '////3/4')).toBe('1/2/3/4');
-  });
-});
