78eaec150d8f6dfe59e3722a971d6e5e4aa87f55
zjx0905 <954270063@qq.com>
2023-04-17 22:07:38 +0800

    ci: 添加文档部署脚本

---
+++ scripts/docs.deploy.ts
@@ -0,0 +1,27 @@
+import consola from 'consola';
+import { exec } from './utils';
+
+main();
+
+function main() {
+  exec('pnpm docs:build');
+
+  console.log('\n');
+  consola.info('Clean');
+  const exist = exec('git branch --list docs', {
+    stdio: 'pipe',
+    encoding: 'utf-8',
+  })
+    .toString()
+    .trim();
+  if (exist) {
+    exec('git branch -D docs');
+  }
+  console.log('\n');
+
+  consola.info('Create docs\n');
+  exec('git branch docs');
+
+  consola.info('Push docs\n');
+  exec('git push origin docs -f');
+}
